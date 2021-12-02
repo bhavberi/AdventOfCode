de93e9b5a37468603772b9314e7a7d1ef2184222
Bhav Beri <43399374+bhavberi@users.noreply.github.com>
2021-12-02 15:55:25 +0530

    Update Day_1-2.py

--- 2021/Day_1-2.py
+++ 2021/Day_1-2.py
@@ -1,1 +1,16 @@
 #Day 1 Part 2
+c=0
+a=2000
+l=list()
+for i in range(a):
+    l+=[int(input()),]
+
+p=l[0]+l[1]+l[2] #For Comparison
+for i in range(1,a-2):
+    s=l[i]+l[i+1]+l[i+2]
+    if s>p:
+        c+=1
+    p=s  
+    
+print(c)
+#Bhav Beri
